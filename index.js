ce86b21dfb8464f9ac5731ddb7a7b375a4da12dd
zuavra <25580993+zuavra@users.noreply.github.com>
2023-09-06 13:44:30 +0300

    First working version, with simplistic key checking in URL query string and in-memory store (Map).

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const framework = require('connect');
+const http = require('http');
+const app = framework();
+const store = new Map();
+
+function logger() {
+    if (process.env.DEBUG === 'yes') {
+        console.log(...arguments);
+    }
+}
+
+app.use('/verify', (req, res, next) => {
+    const URI = req.headers['x-original-uri'];
+    const IP = req.headers['x-forwarded-for'];
+    const now = new Date().getTime();
+    const exp = new Date(now + process.env.VALIDITY_MS).getTime();
+    const logPrefix = `[${now}][${IP}][${URI}]`;
+    logger(`${logPrefix} Processing.`);
+
+    if (store.has(IP)) {
+        const entry = store.get(IP);
+
+        if (now >= (entry?.expirationTimestamp || 0)) {
+            store.delete(IP);
+            res.statusCode = 403;
+            logger(`${logPrefix} Entry found but expired, rejected.`)
+        }
+        else {
+            res.statusCode = 200;
+            logger(`${logPrefix} Entry found valid, allowed.`)
+        }
+    }
+    else {
+        const url = new URL(URI, 'https://ignore.this/');
+        if ('?' + process.env.KEY === url.search) {
+            store.set(IP, {
+                expirationTimestamp: exp,
+            });
+            res.statusCode = 200;
+            logger(`${logPrefix} Key matched, IP added, allowed.`)
+        }
+        else {
+            res.statusCode = 403;
+            logger(`${logPrefix} No key, no IP, rejected.`)
+        }
+    }
+    res.end();
+});
+
+app.use('/ping', (req ,res) => res.end('OK ' + req.socket.remoteAddress));
+
+http.createServer(app).listen(process.env.PORT, process.env.HOST);
+logger(`Listening on ${process.env.HOST}:${process.env.PORT}`);
