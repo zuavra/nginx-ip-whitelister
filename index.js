3001e962743e94d33a29e5587d121047a78ace08
zuavra <25580993+zuavra@users.noreply.github.com>
2023-09-10 00:25:00 +0300

    Make log timestamps humanly readable. Use Node native libraries for extracting values out of query strings. New feature: proxy can specify a key with /verify?key=VALUE parameter.

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 require('dotenv').config();
 const framework = require('connect');
 const http = require('http');
+const QS = require('node:querystring');
+const URL = require('node:url');
+const Logger = require('lib/logger');
+
 const app = framework();
 const store = new Map();
 
-function logger() {
-    if ('yes' === process.env.DEBUG) {
-        console.log(...arguments);
-    }
-}
+app.use('/verify', (req, res) => {
+    const ORIGINAL_URI = req.headers['x-original-uri'];
+    const REMOTE_IP = req.headers['x-forwarded-for'];
 
-app.use('/verify', (req, res, next) => {
-    const URI = req.headers['x-original-uri'];
-    const IP = req.headers['x-forwarded-for'];
-    const now = new Date().getTime();
-    const logPrefix = `[${now}][${IP}][${URI}]`;
+    const VERIFY_OPTIONS = QS.parse(URL.parse(req.url)?.query);
 
-    if (store.has(IP)) {
-        const entry = store.get(IP);
+    const logger = new Logger();
+    logger.addScrubString(process.env.KEY);
+    logger.addScrubString(VERIFY_OPTIONS.key);
+    logger.addPrefix(REMOTE_IP);
+    logger.addPrefix(ORIGINAL_URI, true);
 
+    if (store.has(REMOTE_IP)) {
+        const entry = store.get(REMOTE_IP);
+        const now = new Date().getTime();
         if (now >= parseInt(entry?.expirationTimestamp)) {
-            store.delete(IP);
+            store.delete(REMOTE_IP);
             res.statusCode = 403;
-            logger(`${logPrefix} Entry found but expired, rejected.`)
+            logger.log('IP expired, rejected.');
         }
         else {
             res.statusCode = 200;
-            logger(`${logPrefix} Entry found valid, allowed.`)
+            logger.log('IP found, allowed.');
         }
     }
     else {
-        const url = new URL(URI, 'https://ignore.this/');
-        if ('?' + process.env.KEY === url.search) {
+        const POTENTIAL_URI_KEY = URL.parse(ORIGINAL_URI).query;
+
+        let matched = 0;
+        if (POTENTIAL_URI_KEY === VERIFY_OPTIONS.key) {
+            matched = 1;
+        }
+        else if (POTENTIAL_URI_KEY === process.env.KEY) {
+            matched = 2;
+        }
+
+        if (matched > 0) {
+            const now = new Date().getTime();
             const exp = new Date(now + parseInt(process.env.VALIDITY_MS)).getTime();
-            store.set(IP, {
+            store.set(REMOTE_IP, {
                 expirationTimestamp: exp,
             });
             res.statusCode = 200;
-            logger(`${logPrefix} Key matched, IP added, allowed.`)
+            const keySource = matched === 1 ? 'proxy value' : 'server value';
+            logger.log(`Key matched ${keySource}, IP added, allowed.`);
         }
         else {
             res.statusCode = 403;
-            logger(`${logPrefix} No key, no IP, rejected.`)
+            logger.log('No key, no IP, rejected.');
         }
     }
     res.end();
@@ -58,5 +73,4 @@ app.use('/reject', (req ,res) => {
 });
 
 http.createServer(app).listen(process.env.PORT, process.env.HOST);
-logger(`Listening on ${process.env.HOST}:${process.env.PORT}`);
-logger(process.env);
+console.log(`Listening on ${process.env.HOST}:${process.env.PORT}.`);
