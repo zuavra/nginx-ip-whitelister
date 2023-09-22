27d8b28b57745e73b470b266937c4cf4cfff5aab
zuavra <25580993+zuavra@users.noreply.github.com>
2023-09-22 19:51:31 +0300

    Clean up timeout interval parsing. Leave default up to the calling middleware.

--- middleware/accept_ip.js
+++ middleware/accept_ip.js
@@ -1,10 +1,11 @@
-import timeoutConverter from "../lib/timeout_converter.js";
+const DEFAULT_FIXED_TIMEOUT = 2 * 60 * 60 * 1000;
+import parseInterval from "../lib/parse_interval.js";
 
 export default (_, res) => {
     // all relevant checks passed, add IP to whitelist
     const now = new Date().getTime();
-    const FIXED_TIMEOUT = timeoutConverter(process.env.FIXED_TIMEOUT);
-    const exp = new Date(now + FIXED_TIMEOUT).getTime();
+    const fixedTimeout = parseInterval(process.env.FIXED_TIMEOUT) || DEFAULT_FIXED_TIMEOUT;
+    const exp = new Date(now + fixedTimeout).getTime();
     res.local.store.set(res.local.REMOTE_IP, {
         expirationTimestamp: exp,
     });
