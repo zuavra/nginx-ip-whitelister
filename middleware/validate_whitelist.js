a32ac57a72ef6b6ee0a417ec32fff74ab627e40c
zuavra <25580993+zuavra@users.noreply.github.com>
2023-09-26 11:22:51 +0300

    Code cleanup in whitelist middleware.

--- middleware/validate_whitelist.js
+++ middleware/validate_whitelist.js
@@ -1,29 +1,28 @@
 export default
 (dateFactory) =>
 (_, res) => {
-    if (res.local.store.has(res.local.remoteIP)) {
-        const entry = res.local.store.get(res.local.remoteIP);
-        const now = dateFactory().getTime();
-
-        if (
-            now - entry.createdAt < (res.local.fixedTimeout || 72e5)
-            &&
-            now - entry.lastModifiedAt < (res.local.slidingTimeout || 3e5)
-        ) {
-            res.local.store.set(res.local.remoteIP,
-                Object.assign({}, entry, { lastModifiedAt: now })
-            );
-
-            res.statusCode = 200;
-            res.local.logger.flush('IP found. Allowed.');
-            return res.end();
-        }
-
-        // entry has expired, remove from store, will resume normal checks
-        res.local.store.delete(res.local.remoteIP);
-        res.local.logger.queue('IP found but expired.');
-    }
-    else {
+    const entry = res.local.store.get(res.local.remoteIP);
+    if (!entry) {
         res.local.logger.queue('IP not found.');
+        return;
     }
+
+    const now = dateFactory().getTime();
+    if (
+        now - entry.createdAt < (res.local.fixedTimeout || 72e5)
+        &&
+        now - entry.lastModifiedAt < (res.local.slidingTimeout || 3e5)
+    ) {
+        res.local.store.set(res.local.remoteIP,
+            Object.assign({}, entry, { lastModifiedAt: now })
+        );
+
+        res.statusCode = 200;
+        res.local.logger.flush('IP found. Allowed.');
+        return res.end();
+    }
+
+    // entry has expired, remove from store, will resume normal checks
+    res.local.store.delete(res.local.remoteIP);
+    res.local.logger.queue('IP found but expired.');
 };
