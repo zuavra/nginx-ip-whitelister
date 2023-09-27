7ecdf21d5748b877aecac55204ceeaf11c4fb270
zuavra <25580993+zuavra@users.noreply.github.com>
2023-09-27 23:05:02 +0300

    Simplify for..of variables.

--- middleware/status.js
+++ middleware/status.js
@@ -5,16 +5,16 @@ export default
     const now = dateFactory();
     res.write('<table border="1">');
     res.write('<tr><th>IP<th>Country<th>Created<th>(expires in)<th>Last accessed<th>(expires in)')
-    for (const entry of res.local.store) {
-        res.write(`<tr><td>${entry[0]}`);
-        res.write(`<td>${geoIP.get(entry[0]) || '&mdash;'}`);
-        const createdAt = dateFactory(entry[1].createdAt);
+    for (const [ip, entry] of res.local.store) {
+        res.write(`<tr><td>${ip}`);
+        res.write(`<td>${geoIP.get(ip) || '&mdash;'}`);
+        const createdAt = dateFactory(entry.createdAt);
         res.write(`<td>${createdAt.toLocaleString('sv-SE')}`);
-        res.write('<td>' + humanInterval(entry[1].createdAt + entry[1].fixedTimeout - now));
-        const lastModifiedAt = dateFactory(entry[1].lastModifiedAt);
+        res.write('<td>' + humanInterval(entry.createdAt + entry.fixedTimeout - now));
+        const lastModifiedAt = dateFactory(entry.lastModifiedAt);
         res.write(`<td>${lastModifiedAt.toLocaleString('sv-SE')}`);
-        res.write('<td>' + humanInterval(entry[1].lastModifiedAt + entry[1].slidingTimeout - now));
-        res.write(`<td><a href="/delete?${entry[0]}" title="kick out this IP">[delete]</a>`);
+        res.write('<td>' + humanInterval(entry.lastModifiedAt + entry.slidingTimeout - now));
+        res.write(`<td><a href="/delete?${ip}" title="kick out this IP">[delete]</a>`);
     }
     res.write('</table>');
     res.end();
--- middleware/validate_keys.js
+++ middleware/validate_keys.js
@@ -3,10 +3,10 @@ export default (_, res) => {
         const locatedKey = res.local.accessKeys.indexOf(res.local.visitorKey);
         if (locatedKey !== -1) {
             if (res.local.keyIsolation) {
-                for (const entry of res.local.store) {
-                    if (res.local.visitorKey === entry[1].usedKey) {
+                for (const [ip, entry] of res.local.store) {
+                    if (res.local.visitorKey === entry.usedKey) {
                         res.statusCode = 403;
-                        res.local.logger.flush(`Key is being used by ${entry[0]}. Rejected.`);
+                        res.local.logger.flush(`Key is being used by ${ip}. Rejected.`);
                         return res.end();
                     }
                 }
