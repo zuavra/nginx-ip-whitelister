bed750b6555dc2b989aa9b5fa89ae2f0f2da182e
zuavra <25580993+zuavra@users.noreply.github.com>
2026-09-15 17:55:17 +0300

    Wrap tests in await's to avoid ECONNRESET race conditions.

--- testing/index.js
+++ testing/index.js
@@ -1,67 +1,101 @@
+import appMaker from '../lib/app.js';
 import supertest from 'supertest';
 
-const finish = (description) => (e, _) => {
-    if (e) console.log(`${description} [FAIL]: ${e.message}`);
-    else console.log(`${description} [PASS]`);
-};
+const request = (logLevel) => supertest(
+    appMaker(
+        logLevel || 'crit',
+        ['debug', 'info', 'notice', 'warn', 'error', 'crit']
+    ).__server
+);
 
-const request = supertest('http://localhost:3000');
+const check = (description, P) => {
+    P.then(_ => { if (description) console.log(`${description} [PASS]`); })
+        .catch(e => console.log(`${description} [FAIL]: ${e.message}`));
+    return P;
+}
 
-request
-    .get('/approve')
-    .expect(200, "APPROVED")
-    .end(finish("/approve responds 200"));
+await check("/approve responds 200",
+    request()
+        .get('/approve')
+        .expect(200, "APPROVED")
+);
 
-request
-    .get('/reject')
-    .expect(403, "REJECTED")
-    .end(finish("/reject responds 403"));
+await check("/reject responds 403",
+    request()
+        .get('/reject')
+        .expect(403, "REJECTED")
+);
 
-request
-    .get('/')
-    .expect(404, "NOT FOUND")
-    .end(finish("root path responds 404"));
+await check("/ responds 404",
+    request()
+        .get('/')
+        .expect(404, "NOT FOUND")
+);
 
-request
-    .get('/' + (Math.random() + 1).toString(36).substring(7))
-    .expect(404, "NOT FOUND")
-    .end(finish("random path responds 404"));
+await check("random path responds 404",
+    request()
+        .get('/' + (Math.random() + 1).toString(36).substring(7))
+        .expect(404, "NOT FOUND")
+);
 
-request
-    .post('/')
-    .expect(405, "METHOD NOT ALLOWED")
-    .end(finish("POST method responds 405"));
+await check("POST method responds 405",
+    request()
+        .post('/')
+        .expect(405, "METHOD NOT ALLOWED")
+);
 
-request
-    .get('/verify')
-    .expect(200)
-    .end(finish("/verify without config responds 200"));
+await check("/verify without config responds 200",
+    request()
+        .get('/verify')
+        .expect(200)
+);
 
-request
-    .get('/verify')
-    .set('x-nipw-netmask-allow', '8.8.8.8/24')
-    .expect(403)
-    .end(finish("/verify with legacy netmask-allow header responds 403"));
+await check("/verify with legacy netmask-allow header responds 403",
+    request()
+        .get('/verify')
+        .set('x-nipw-netmask-allow', '8.8.8.8/24')
+        .expect(403)
+);
 
-request
-    .get('/verify')
-    .set('x-nipw-netmask-deny', '8.8.8.8/24')
-    .expect(403)
-    .end(finish("/verify with legacy netmask-deny header responds 403"));
+await check("/verify with legacy netmask-deny header responds 403",
+    request()
+        .get('/verify')
+        .set('x-nipw-netmask-deny', '8.8.8.8/24')
+        .expect(403)
+);
 
-request
-    .get('/verify')
-    .set('x-original-uri', '/?GoodKey3')
-    .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
-    .expect(200)
-    .end(finish("/verify with key set and good key responds 200"));
+await check("/verify with good key responds 200",
+    request()
+        .get('/verify')
+        .set('x-original-uri', '/?GoodKey3')
+        .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+        .expect(200)
+);
 
-request
-    .get('/verify')
-    .set('x-original-uri', '/?BadKey')
-    .set('x-nipw-key', 'GoodKey')
-    .expect(403)
-    .end(finish("/verify with key set and bad key responds 403"));
+await check("/verify with bad key responds 403",
+    request()
+        .get('/verify')
+        .set('x-original-uri', '/?BadKey')
+        .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+        .expect(403)
+);
 
+(async () => {
+    const R = request();
+    await check(null,
+        R.get('/verify')
+        .set('x-forwarded-for', '1.2.3.4')
+        .set('x-original-uri', '/?GoodKey3')
+        .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+        // .expect(200) # FIXME: how to verify setup queries
+    );
+    await check('/verify with same IP after good key responds 200',
+        R.get('/verify')
+        .set('x-forwarded-for', '1.2.3.4')
+        .set('x-original-uri', '/')
+        .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+        .expect(200)
+    );
+})();
 
 // good key + logout + attempt => 403
