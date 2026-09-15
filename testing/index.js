3d41c5c9fe86d02119dbbe5ebb0a0efe1a5ca86d
zuavra <25580993+zuavra@users.noreply.github.com>
2026-09-15 18:34:08 +0300

    Add ability to chain tests to create prerequisites for complex setups. Add more tests.

--- testing/index.js
+++ testing/index.js
@@ -8,94 +8,122 @@ const request = (logLevel) => supertest(
     ).__server
 );
 
-const check = (description, P) => {
-    P.then(_ => { if (description) console.log(`${description} [PASS]`); })
-        .catch(e => console.log(`${description} [FAIL]: ${e.message}`));
-    return P;
+async function testcase(description, tests, logLevel) {
+    const R = request(logLevel);
+    for (let i = 0; i < tests.length; i++) {
+        const isLast = !(i < tests.length - 1);
+        try {
+            await tests[i](R);
+            if (isLast) {
+                console.log(`${description} [PASS#${i + 1}/${tests.length}]`);
+            }
+        } catch (err) {
+            console.log(`${description} [FAIL@${i + 1}/${tests.length}]: ${err.message}`)
+            break;
+        }
+    }
 }
 
-await check("/approve responds 200",
-    request()
+await testcase("/approve responds 200",
+[
+    R => R
         .get('/approve')
         .expect(200, "APPROVED")
-);
+]);
 
-await check("/reject responds 403",
-    request()
+await testcase("/reject responds 403",
+[
+    R => R
         .get('/reject')
         .expect(403, "REJECTED")
-);
+]);
 
-await check("/ responds 404",
-    request()
+await testcase("/ responds 404",
+[
+    R => R
         .get('/')
         .expect(404, "NOT FOUND")
-);
+]);
 
-await check("random path responds 404",
-    request()
+await testcase("random path responds 404",
+[
+    R => R
         .get('/' + (Math.random() + 1).toString(36).substring(7))
         .expect(404, "NOT FOUND")
-);
+]);
 
-await check("POST method responds 405",
-    request()
+await testcase("POST method responds 405",
+[
+    R => R
         .post('/')
         .expect(405, "METHOD NOT ALLOWED")
-);
+]);
 
-await check("/verify without config responds 200",
-    request()
+await testcase("/verify without config responds 200",
+[
+    R => R
         .get('/verify')
         .expect(200)
-);
+]);
 
-await check("/verify with legacy netmask-allow header responds 403",
-    request()
+await testcase("/verify with legacy netmask-allow header responds 403",
+[
+    R => R
         .get('/verify')
         .set('x-nipw-netmask-allow', '8.8.8.8/24')
         .expect(403)
-);
+]);
 
-await check("/verify with legacy netmask-deny header responds 403",
-    request()
+await testcase("/verify with legacy netmask-deny header responds 403",
+[
+    R => R
         .get('/verify')
         .set('x-nipw-netmask-deny', '8.8.8.8/24')
         .expect(403)
-);
+]);
 
-await check("/verify with good key responds 200",
-    request()
+await testcase("/verify with good key responds 200",
+[
+    R => R
         .get('/verify')
         .set('x-original-uri', '/?GoodKey3')
         .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
         .expect(200)
-);
+]);
 
-await check("/verify with bad key responds 403",
-    request()
+await testcase("/verify with bad key responds 403",
+[
+    R => R
         .get('/verify')
         .set('x-original-uri', '/?BadKey')
         .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
         .expect(403)
-);
+]);
 
-(async () => {
-    const R = request();
-    await check(null,
-        R.get('/verify')
-        .set('x-forwarded-for', '1.2.3.4')
-        .set('x-original-uri', '/?GoodKey3')
+await testcase('/verify with same IP after good key responds 200',
+[
+    R => R.get('/verify')
+            .set('x-forwarded-for', '1.2.3.4')
+            .set('x-original-uri', '/?GoodKey3')
+            .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+            .expect(200),
+    R => R.get('/verify')
+            .set('x-forwarded-for', '1.2.3.4')
+            .set('x-original-uri', '/')
+            .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+            .expect(200)
+]);
+
+await testcase("/verify with bad key but IP is excluded responds 200",
+[
+    R => R
+        .get('/verify')
         .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
-        // .expect(200) # FIXME: how to verify setup queries
-    );
-    await check('/verify with same IP after good key responds 200',
-        R.get('/verify')
+        .set('x-original-uri', '/?BadKey')
         .set('x-forwarded-for', '1.2.3.4')
-        .set('x-original-uri', '/')
-        .set('x-nipw-key', ['GoodKey1', 'GoodKey2', 'GoodKey3'])
+        .set('x-nipw-ip-exclude', '1.2.3.4')
         .expect(200)
-    );
-})();
+]);
 
-// good key + logout + attempt => 403
+// key isolation
+// different whitelists
